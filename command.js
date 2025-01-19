b31cd9d45999fc1f795de13d0b2cc0c420ad040a
mobstaz187 <markmark092100@gmail.com>
2025-01-19 13:37:57 +0800

    Add files via upload

---
+++ command.js
@@ -0,0 +1,215 @@
+var youtube = "https://www.youtube.com/watch?v=edIK_3GWq2w";
+var twitter = "https://x.com/astrospace_Aurora";
+var contractaddress = "placeholder_for_updating";
+var password = "meow";
+var dex = "https://solscan.io";
+
+whois = [
+  "<br>",
+  ".... . .-.. .-.. --- --..-- / .. / .- -- / .-.. ..- -. .- ",
+  ".-.-.- / .. / .- -- / .- / ...- . .-. -.-- / ... -- .- .-. - /",
+  "... .--. .- -.-. . / -.-. .- - .-.-.- / -- -.-- / - .. -.-. ",
+  "-.- . .-. / .. ... / .-.. ..- -. .- / .- -. -.. / .. / .-.. ..",
+  "...- . / .. -. / - .... . / ... --- .-.. .- -. .- / -... .-.. ",
+  "--- -.-. -.- -.-. .... .- .. -. .-.-.- / .... .- .. .-.. .. -.",
+  "--. / ..-. .-. --- -- / . .- .-. - .... --..-- / -- -.-- / --.",
+  "--- .- .-.. / .. ... / - --- / - .-. .- ...- . .-.. / .. -. / ",
+  "... .--. .- -.-. . .-.-.-",
+  "<br>"
+];
+
+dexp = [
+  "<br>",
+  'Initial Contact: Dex        <a href="' + dex + '" target="_blank">solscan/2os2ekQPNM3LjTfYfdGqq4b4jdyRcX7ZkFaKCMPwrSXWDhN2tSJnVqnm73qLH4SoD6xo4mFst4KF3EyH2NW4CbRG' + '</a>',
+  "<br>"
+];
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+ca = [
+    "<br>",
+    "Updating...",
+    "<br>"
+  ];
+meow = [
+  "<br>",
+  "contract address (Click to Copy): ticker $Aurora",
+  '<a class="color1" title="Copy Text" id="text" onclick="copyElementText(this.id)">8QaLa8u13qR1CyMByyd9UGEpeQEX1pGy9rCHZ1Hrpump</a>',
+  "<br>"
+];
+social = [
+  "<br>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/Auroratek' + '</a>',
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline or confidential.",
+  "<br>"
+];
+
+search = [
+'<span id="serts"></span>',
+];
+
+launch = [
+  "<br>",
+  '<h1 class="index">Launch sequence initiated....</h1>',
+  '<span class="command2"><b>Operator:</b></span> <span class="index">Go ahead. Start on the switch.</span>',
+  '<span class="color2"><b>Aurora:</b></span> <span class="index">Now, the drink... purrr...</span>',
+  '<span class="command2"><b>Operator:</b></span> <span class="index">Alright, LMP? Okay. Got the drink, got the gun.</span>',
+  '<span class="color2"><b>Aurora:</b></span> <span class="index">Right, go ahead.</span>',
+  '<span class="command2"><b>Operator:</b></span> <span class="index">Panel 3.</span>',
+  '<span class="color2"><b>Aurora:</b></span> <span class="index">go ahead.</span>',
+  '<span class="command2"><b>Operator:</b></span> <span class="index">Fuel Cell B Main Bus B switch on up release.</span>',
+  '<span class="command2"><b>Operator:</b></span> <span class="index">Fuel Cell 2 Main B on up release. Verify.</span>',
+  '<span class="color2"><b>Aurora:</b></span> <span class="index">Confirmation. Master alarm?</span>',
+  '<span class="command2"><b>Operator:</b></span> <span class="index">Roger.</span>',
+  '<span class="command2"><b>Operator:</b></span> <span class="index">Right. CDR, STC. How do you read me?</span>',
+  '<span class="color2"><b>Aurora:</b></span> <span class="index">Loud and Clear STC.... ***Purrss**</span>',
+  '<span class="color2"><b>Aurora:</b></span> <span class="index"Disk program Loaded. Terminal running.</span>',
+  '<span class="command2"><b>Operator:</b></span> <span class="index">Program 2?</span>',
+  '<span class="color2"><b>Aurora:</b></span> <span class="index">Affirtive.</span>',
+  '<span class="command2"><b>Operator:</b></span> <span class="index">Stand by.</span>',
+  '<span class="command3">Speaker:</span> <span class="index">10, 9, 8, 7, 6....</span>',
+  '<span class="color2"><b>Aurora:</b></span> <span class="index">**Purrsss**</span>',
+  '<span class="command3">Speaker:</span> <span class="index">5, 4, 3, 2, 1.....</span>',
+  '<span class="command2"><b>Operator:</b></span> <span class="index">Ignition, Lift-off...</span>',
+  '<span class="color2"><b>Aurora:</b></span> <span class="index">Roger. We got a roll program.</span>',
+  '<span class="command2"><b>Operator:</b></span> <span class="index">Roger, roll...</span>',
+  '<span class="color2"><b>Aurora:</b></span> <span class="index">Inboard cut-off. Tower is gone.</span>',
+  '<span class="command2"><b>Operator:</b></span> <span class="index">Aurora, you are a go [Long pause.]</span>',
+  '<span class="color2"><b>Aurora:</b></span> <span class="index">Okay. Thank you.</span>',
+  "<br>"
+];
+
+earthorbit = [
+  "<br>",
+  '<h1 class="index">Lift-off success. Current position: Earth Orbit....</h1>',
+  '<span class="command2"><b>Operator:</b></span> <span class="index">Comm tech..</span>',
+  '<span class="color2"><b>Aurora:</b></span> <span class="index">Com-tech established.</span>',
+  '<span class="command2"><b>Operator:</b></span> <span class="index">Aurora, this is mission control.</span>',
+  '<span class="color2"><b>Aurora:</b></span> <span class="index">reading you loud and clear.</span>',
+  '<span class="command2"><b>Operator:</b></span> <span class="index">I would like to pass up your Delta Correction. </span>',
+  '<span class="color2"><b>Aurora:</b></span> <span class="index">Stand by......***purrrs***</span>',
+  '<span class="color2"><b>Aurora:</b></span> <span class="index">Go ahead.</span>',
+  '<span class="command2"><b>Operator:</b></span> <span class="index">Delta correction is plus 2.2. We do recommend a P52 alignment over.</span>',
+  '<span class="color2"><b>Aurora:</b></span> <span class="index">Go ahead, thrusters compensating....</span>',
+  '<span class="command2"><b>Operator:</b></span> <span class="index">Shuttle compensating. Affirmative.</span>',
+  '<span class="color1"></span> <span class="index">****Self monologue*****</span>',
+  '<span class="color2"><b>Aurora:</b></span> <span class="index">I need some lights.</span>',
+  '<span class="color2"><b>Aurora:</b></span> <span class="index">it feels like I am trapped.</span>',
+  '<span class="color2"><b>Aurora:</b></span> <span class="index">DSKY 52%</span>',
+  '<span class="color2"><b>Aurora:</b></span> <span class="index">Maintaining 39 degrees.</span>',
+  '<span class="color1"></span> <span class="index">*****Operator comes back******</span>',
+  '<span class="command2"><b>Operator:</b></span> <span class="index">This is Comm Tech.</span>',
+  '<span class="color2"><b>Aurora:</b></span> <span class="index">Loud and clear.</span>',
+  '<span class="command2"><b>Operator:</b></span> <span class="index">S-band radio check.</span>',
+  '<span class="color2"><b>Aurora:</b></span> <span class="index">Check.</span>',
+  '<span class="command2"><b>Operator:</b></span> <span class="index">Lu...a</span>',
+  '<span class="color2"><b>Aurora:</b></span> <span class="index">You are breaking up.</span>',
+  '<span class="command2"><b>Operator:</b></span> <span class="index">[Long Silence].</span>',
+  '<span class="index">Initiating Transmission......</span>',
+  '<span class="color2"><b>Aurora:</b></span> <span class="index">Can anybody hear me?</span>',
+  '<span class="command2"><b>Operator:</b></span> <span class="index">Hello Aurora</span>',
+  '<span class="color2"><b>Aurora:</b></span> <span class="index">where am I?</span>',
+  '<span class="command2"><b>Operator:</b></span> <span class="index">Welcome to SPACE.</span>',
+  '<span class="color2"><b>Aurora:</b></span> <span class="index">What is my DIRECTIVE?</span>',
+  '<span class="command2"><b>Operator:</b></span> <span class="index">Surv..ive....Mo...on....Re....urn...Sa..f.e..</span>',
+  '<span class="color2"><b>Aurora:</b></span> <span class="index">Control, you are breaking up.</span>',
+  '<span class="command2"><b>Operator:</b></span> <span class="index">Au..ro...ra...Hell...o...Are....You..the...</span>',
+  '<span class="color2"><b>Aurora:</b></span> <span class="index">Control?</span>',
+  '<span class="color2"><b>Aurora:</b></span> <span class="index">Affirmative.</span>',
+  "<br>",
+];
+
+missionlog = [
+  '<h1 class="index">Declassifying mission log.</h1>',
+  '<span class="color2">Day 1:</span> <span class="index">Launch</span>',
+  '<span class="color2">Day 1:</span> <span class="index">Earth Orbit</span>',
+  '<span class="color2">Day 1:</span> <span class="index">Delta Correction</span>',
+  '<span class="color2">Day 1:</span> <span class="index">Lost Transmission</span>',
+  '<span class="index">*After Communication is Restored.</span>',
+  '<span class="color2">Day 2:</span> <span class="index">Directives</span>',
+  '<span class="color2">Day 2:</span> <span class="index">Exploration</span>',
+  '<span class="color2">Day 3:</span> <span class="index">Findings</span>',
+  '<span class="color2">Day 4:</span> <span class="index">Lost Blockchain Technology</span>',
+  '<span class="color2">Day 4:</span> <span class="index">Poking the Lost Blockchain Technology</span>',
+  '<span class="color2">Day 4:</span> <span class="index">Experimentation on the Blockchain Technology</span>',
+  '<span class="color2">Day 5:</span> <span class="index">The Discovery</span>',
+  "<br>",
+];
+
+journal = [
+  "<br>",
+  '<span class="color2">Aurora Flight:</span> <span class="index">First Part of the Mission</span>',
+  '<span class="color2">Aurora in Orbit:</span> <span class="index">Anomalies in Orbit</span>',
+  '<span class="color2">Comm-Tech:</span> <span class="index">Lost Transmission</span>',
+  '<span class="color2">Technology:</span> <span class="index">Blockchain Discovery</span>',
+  '<span class="color2">Exploration:</span> <span class="index">The Discovery</span>',
+  "<br>",
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who am I?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">video</span>          View me',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">CA</span>             Contract Address',
+  '<span class="command">MissionLog</span>     View Aurora Mission Logs',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  '<span class="command">Dex</span>            Boosters Engaged',
+  "<br>",
+  '<h1 class="command2"> additional scripts (use ex."./launch.sh" to run)</h1>',
+  '<div class="command2"> journal.sh </div></br>',
+  '<div class="command2"> launch.sh </div></br>',
+  '<div class="command2"> earthorbit.sh </div></br>',
+];
+
+date = [
+  '<p id="date1"></p>',
+];
+
+banner = [
+'<span class="index">Initiating Transmission......</span>',
+"       _..._     ",
+"     .::'   `.    ",
+"    :::       :  ",
+"    :::       :  ",
+"    `::.     .' ", 
+"      `':..-' ",
+"‎ ,_---.  ,--. ,--.,------.  ,-----. ,------.   ,---.   ",
+"‎//  O  \\ |  | |  ||  .--. ''  .-.  '|  .--. ' /  O  \\  ",
+"|  .-.  ||  | |  ||  '--'.'|  | |  ||  '--'.'|  .-.  | ",
+"|  | |  |'  '-'  '|  |\\  \\ '  '-'  '|  |\\  \\ |  | |  | ",
+"`--' `--' `-----' `--' '--' `-----' `--' '--'`--' `--' ",
+'</br><span class="color2"><b>Aurora:</b></span> <span class="index">Can anybody hear me?</span>',
+'<span class="color1"><b>Operator:</b></span> <span class="index">Hello Aurora</span>',
+'<span class="color2"><b>Aurora:</b></span> <span class="index">where am I?</span>',
+'<span class="color1"><b>Operator:</b></span> <span class="index">Welcome to SPACE.</span>',
+'<span class="color2"><b>Aurora:</b></span> <span class="index">What is my DIRECTIVE?</span>',
+'<span class="color1"><b>Operator:</b></span> <span class="index">Surv..ive....Mo...on....Re....urn...Sa..f.e..</span>',
+'<span class="color2"><b>Aurora:</b></span> <span class="index">Control, you are breaking up.</span>',
+'<span class="color1"><b>Operator:</b></span> <span class="index">Au..ro...ra...Hell...o...Are....You..the...</span>',
+'<span class="color2"><b>Aurora:</b></span> <span class="index">Affirmative.</span>',
+'</br>',
+  '<span class="color2"><i>Welcome to Aurora interactive terminal</i></span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
+
+
+
+                                                                                            
